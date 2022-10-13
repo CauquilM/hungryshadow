f7c5852577b9aff5def868ba59e1b566438905dd
MathCoder <mathieufc31@gmail.com>
2022-10-13 07:21:29 +0200

    put code from app to home

--- src/App.vue
+++ src/App.vue
@@ -21,59 +21,15 @@
         <span class="mr-2 black--text">Se connecter</span>
       </v-btn>
     </v-app-bar>
-
     <v-main>
-      <v-container fluid class="grey lighten-5 fill-height">
-        <v-row no-gutters class="align-center">
-          <v-col cols="6">
-            <DisplayHungerComponent class="text-center sticky-component" />
-          </v-col>
-          <v-col>
-            <v-divider vertical class="divider" />
-          </v-col>
-          <v-col cols="5" class="text-center">
-            <div v-if="isConnected">
-              <FeedingPostList />
-            </div>
-            <h1 v-else class="text-lg-h4">
-              Bienvenue sur l'application pour nourrir Shadow
-            </h1>
-          </v-col>
-        </v-row>
-      </v-container>
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import DisplayHungerComponent from "./components/DisplayHungerComponent.vue";
-import FeedingPostList from "./components/FeedingPostList.vue";
 export default {
   name: "App",
-  data: () => ({
-    isConnected: true,
-  }),
-  components: { DisplayHungerComponent, FeedingPostList },
+  
 };
 </script>
-<style>
-.divider {
-  border-width: 4px !important;
-  border-color: black !important;
-  position: fixed;
-  top: 0;
-}
-
-.sticky-component {
-  width: 400px;
-  position: fixed;
-  top: 50%;
-  z-index: 1;
-  /* centering */
-  left: 25%;
-  transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  -webkit-transform: translate(-50%, -50%);
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,52 @@
 <template>
-  <div class="home">
-    
-  </div>
+    <v-container fluid class="grey lighten-5 fill-height">
+      <v-row no-gutters class="align-center">
+        <v-col cols="6">
+          <DisplayHungerComponent class="text-center sticky-component" />
+        </v-col>
+        <v-col>
+          <v-divider vertical class="divider" />
+        </v-col>
+        <v-col cols="5" class="text-center">
+          <div v-if="isConnected">
+            <FeedingPostList />
+          </div>
+          <h1 v-else class="text-lg-h4">
+            Bienvenue sur l'application pour nourrir Shadow
+          </h1>
+        </v-col>
+      </v-row>
+    </v-container>
 </template>
 
 <script>
+import DisplayHungerComponent from "@/components/DisplayHungerComponent.vue";
+import FeedingPostList from "@/components/FeedingPostList.vue";
 export default {
   name: "Home",
-  components: {},
+  components: { DisplayHungerComponent, FeedingPostList },
+  data: () => ({
+    isConnected: true,
+  }),
 };
 </script>
+<style>
+.divider {
+  border-width: 4px !important;
+  border-color: black !important;
+  position: fixed;
+  top: 0;
+}
+
+.sticky-component {
+  width: 400px;
+  position: fixed;
+  top: 50%;
+  z-index: 1;
+  /* centering */
+  left: 25%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+}
+</style>
