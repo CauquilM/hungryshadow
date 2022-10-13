e4a62db733f5fc66ea4246d8675d2d04e7a4a398
MathCoder <mathieufc31@gmail.com>
2022-10-13 07:25:48 +0200

    add comment feature on card

--- src/components/FeedingPostList.vue
+++ src/components/FeedingPostList.vue
@@ -4,23 +4,28 @@
     <div v-for="post in posts" :key="post.index" data-cy="list-length">
       <v-card class="mx-auto mt-10" max-width="344">
         <v-card-text>
-          <p class="text-h6 text--primary">Shadow a eu un repas à {{post.time}}</p>
-          <p class="text-h6 text--primary">La dose était {{post.quantity}}</p>
           <p class="text-h6 text--primary">
-            La personne qui à donnée est {{post.person}}
+            Shadow a eu un repas à {{ post.time }}
+          </p>
+          <p class="text-h6 text--primary">La dose était {{ post.quantity }}</p>
+          <p class="text-h6 text--primary">
+            La personne qui à donnée est {{ post.person }}
+          </p>
+          <p class="text-h6 text--primary" v-if="post.comment">
+            {{ post.comment }}
           </p>
           <!-- Pour les commentaires <p></p> -->
         </v-card-text>
         <v-card-actions>
-      <v-btn
-        data-cy="list-button"
-        text
-        color="deep-purple accent-4"
-        @click="deletePost(post.index)"
-      >
-        Supprimer
-      </v-btn>
-    </v-card-actions>
+          <v-btn
+            data-cy="list-button"
+            text
+            color="deep-purple accent-4"
+            @click="deletePost(post.index)"
+          >
+            Supprimer
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </div>
   </div>
@@ -30,15 +35,21 @@ export default {
   name: "FeedingPostList",
   data() {
     return {
-      posts: [{ time: "17:15", quantity: "petite", person: "Maman" },
-      { time: "10:00", quantity: "beaucoup", person: "Mathieu" },
-    ],
+      posts: [
+        {
+          time: "17:15",
+          quantity: "petite",
+          person: "Maman",
+          comment: "La petite à bien mangée",
+        },
+        { time: "10:00", quantity: "beaucoup", person: "Mathieu" },
+      ],
     };
   },
   methods: {
-    deletePost(e){
-        this.posts.splice(this.posts.indexOf(e), 1);
-    }
+    deletePost(e) {
+      this.posts.splice(this.posts.indexOf(e), 1);
+    },
   },
 };
 </script>
